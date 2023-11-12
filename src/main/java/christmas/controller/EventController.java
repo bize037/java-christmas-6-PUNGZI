49b47dd44e9cf7da5add9501fc0b81d8d4f6534c
PUNGZI <ashyoo1367@gmail.com>
2023-11-12 16:10:38 +0900

    refactor(EventController): 입력 메서드 재정리

--- src/main/java/christmas/controller/EventController.java
+++ src/main/java/christmas/controller/EventController.java
@@ -3,32 +3,40 @@ package christmas.controller;
 import christmas.domain.MenuAndTotalNumber;
 import christmas.domain.VisitDate;
 import christmas.view.InputView;
+import java.util.HashMap;
 
 public class EventController {
     InputView inputView = new InputView();
 
+    private VisitDate visitDate;
+    private MenuAndTotalNumber menuAndTotalNumber;
+
     public void startEvent() {
-        inputView.inputVisitDateMessage();
+        inputEvent();
+    }
+
+    private void inputEvent() {
+        inputView.visitDateMessage();
         generateVisitDate();
-        inputView.inputOrderMessage();
-        generateOrder();
+        inputView.menuAndTotalNumberMessage();
+        generateMenuAndTotalNumber();
     }
 
     private void generateVisitDate() {
         try {
-            new VisitDate(inputView.inputConsole());
+            visitDate = new VisitDate(inputView.inputConsole());
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
             generateVisitDate();
         }
     }
 
-    private void generateOrder() {
+    private void generateMenuAndTotalNumber() {
         try {
-            new MenuAndTotalNumber(inputView.inputConsole());
+            menuAndTotalNumber = new MenuAndTotalNumber(inputView.inputConsole());
         } catch (IllegalArgumentException e) {
             System.out.println(e.getMessage());
-            generateOrder();
+            generateMenuAndTotalNumber();
         }
     }
 }
