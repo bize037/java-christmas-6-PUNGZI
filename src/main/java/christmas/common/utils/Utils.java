a221f4667f347d9807e5511532a22267c2f8ccdd
PUNGZI <ashyoo1367@gmail.com>
2023-11-14 00:58:24 +0900

    refactor(Utils): 클래스 재선언 방지를 위해 util에 decimal 추가

--- src/main/java/christmas/common/utils/Utils.java
+++ src/main/java/christmas/common/utils/Utils.java
@@ -1,12 +1,24 @@
 package christmas.common.utils;
 
+import java.text.DecimalFormat;
 import java.util.Arrays;
 import java.util.List;
 
 public class Utils {
+    private static DecimalFormat dec = new DecimalFormat("###,###");
+    private static final int MAKE_MINUS = -1;
+
     public static List<String> splitInList(String input, String splitSymbol) {
         return Arrays.stream(input.split(splitSymbol))
                 .map(String::trim)
                 .toList();
+    }
+
+    public static String decFormat(int input) {
+        return dec.format(input);
+    }
+
+    public static String minusDecformat(int input) {
+        return dec.format(input * MAKE_MINUS);
     }
 }
