d7b85cac4dc6c9b3953b68af15a934bebdc2b55d
PUNGZI <ashyoo1367@gmail.com>
2023-11-13 16:56:52 +0900

    feat(Benefit): 평일 및 주말 할인 내역 출력 기능

--- src/main/java/christmas/domain/Benefit.java
+++ src/main/java/christmas/domain/Benefit.java
@@ -11,9 +11,55 @@ public class Benefit {
     private final int beforeSaleTotalPay;
 
     public Benefit(int date, HashMap<String, Integer> menusAndTotalNumbers, int beforeSaleTotalPay) {
+        dayOfWeekSale(dayOfWeek(date));
+
         this.date = date;
         this.menusAndTotalNumbers = menusAndTotalNumbers;
         this.beforeSaleTotalPay = beforeSaleTotalPay;
+    }
+
+    private String dayOfWeek(int date) {
+        List<String> dayOfWeekNames = DayOfWeek.getAllDayOfWeek().stream().map(DayOfWeek::getWeek).toList();
+        return dayOfWeekNames.get(date % 7 - 1);
+    }
+
+    public String dayOfWeekSale(String dayOfWeek) {
+        if (dayOfWeek.equals(DayOfWeek.FRIDAY.getWeek()) || dayOfWeek.equals(DayOfWeek.SATURDAY.getWeek())) {
+            return "주말 할인: -" + weekendSale();
+        }
+        return "평일 할인: -" + weekdaySale();
+    }
+
+    public int weekdaySale() {
+        int price = 0;
+        for (HashMap.Entry<String, Integer> order : menusAndTotalNumbers.entrySet()) {
+            addWeekdaySale(order, price);
+        }
+        return price;
+    }
+
+    private void addWeekdaySale(HashMap.Entry<String, Integer> order, int price) {
+        for (MenuAndPrice menuAndPrice : MenuAndPrice.getDesserts()) {
+            if (menuAndPrice.getMenu().equals(order.getKey())) {
+                price += 2023;
+            }
+        }
+    }
+
+    private int weekendSale() {
+        int price = 0;
+        for (HashMap.Entry<String, Integer> order : menusAndTotalNumbers.entrySet()) {
+            addWeekendSale(order, price);
+        }
+        return price;
+    }
+
+    private void addWeekendSale(HashMap.Entry<String, Integer> order, int price) {
+        for (MenuAndPrice menuAndPrice : MenuAndPrice.getMains()) {
+            if (menuAndPrice.getMenu().equals(order.getKey())) {
+                price += 2023;
+            }
+        }
     }
 
     private int checkChristmasSale() {
