91dbc01e6733d57b4ddb7c254f90b5cd81b30f4c
PUNGZI <ashyoo1367@gmail.com>
2023-11-14 19:27:29 +0900

    Test(BadgeTest): 금액에 따라 배지 이름을 다르게 출력하는지 테스트

---
+++ src/test/java/christmas/domain/BadgeTest.java
@@ -0,0 +1,16 @@
+package christmas.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class BadgeTest {
+    @DisplayName("금액에 따라 배지 이름을 다르게 출력하는가")
+    @CsvSource(value = {"0, '없음'", "4900, '없음'", "5000, '별'", "9900, '별'", "10000, '트리'", "19900, '트리'", "20000, '산타'", "39900, '산타'"}, delimiter = ',')
+    @ParameterizedTest
+    void starBadgeTest(int price, String badge) {
+        String badgeName = Badge.getBadge(price);
+        Assertions.assertEquals(badgeName, badge);
+    }
+}
