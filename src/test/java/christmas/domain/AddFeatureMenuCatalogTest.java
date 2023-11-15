986514b39dc1dda901cdd8cf9f7894918af21ce6
PUNGZI <ashyoo1367@gmail.com>
2023-11-15 19:44:35 +0900

    test(AddFeatureMenuCatalogTest): 샴페인 증정 금액 미출력 테스트

--- src/test/java/christmas/domain/AddFeatureMenuCatalogTest.java
+++ src/test/java/christmas/domain/AddFeatureMenuCatalogTest.java
@@ -2,6 +2,7 @@ package christmas.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import christmas.common.constants.Unit;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -12,15 +13,22 @@ public class AddFeatureMenuCatalogTest {
     기능에 대한 자세한 설명은 README의 추가 출력 사항 항목 참고
     */
 
-    private static final String MORE_NEED_PAY = "11,000원";
-
     @DisplayName("입력한 메뉴의 가격 합이 95,000원 이상 120,000원 이하일 때 얼마를 더 결제하면 샴페인을 받을 수 있는지 정상적으로 출력")
     @ValueSource(strings = {"티본스테이크-1,바비큐립-1"})
     @ParameterizedTest
     void presentMenuMoreNeedPayTest(String menuAndTotalNumber) {
         MenuCatalog menuCatalog = new MenuCatalog(menuAndTotalNumber);
         menuCatalog.beforeSaleTotalPay();
-        assertThat(menuCatalog.presentMenuMoreNeedPay()).contains(MORE_NEED_PAY);
+        assertThat(menuCatalog.presentMenuMoreNeedPay()).contains(Unit.KOREA_MONEY.getUnit());
 
+    }
+
+    @DisplayName("입력한 메뉴의 가격 합이 95,000원 이상 120,000원 이하가 아니면 안내문구 미출력")
+    @ValueSource(strings = {"양송이수프-2,타파스-1", "바비큐립-2,레드와인-2"})
+    @ParameterizedTest
+    void presentMenuMoreNeedPayNotPrintTest(String menuAndTotalNumber) {
+        MenuCatalog menuCatalog = new MenuCatalog(menuAndTotalNumber);
+        menuCatalog.beforeSaleTotalPay();
+        assertThat(menuCatalog.presentMenuMoreNeedPay()).doesNotContain(Unit.KOREA_MONEY.getUnit());
     }
 }
