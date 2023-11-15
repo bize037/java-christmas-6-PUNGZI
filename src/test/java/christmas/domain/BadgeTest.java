3bd9628e8df151d2f2a66acce185335d43de010c
PUNGZI <ashyoo1367@gmail.com>
2023-11-15 17:12:52 +0900

    refactor(BadgeTest): 가독성을 위해 변수 이름 변경

--- src/test/java/christmas/domain/BadgeTest.java
+++ src/test/java/christmas/domain/BadgeTest.java
@@ -9,8 +9,8 @@ public class BadgeTest {
     @DisplayName("금액에 따라 배지 이름을 다르게 출력하는가")
     @CsvSource(value = {"0, '없음'", "4900, '없음'", "5000, '별'", "9900, '별'", "10000, '트리'", "19900, '트리'", "20000, '산타'", "39900, '산타'"}, delimiter = ',')
     @ParameterizedTest
-    void starBadgeTest(int price, String badge) {
-        String badgeName = Badge.getBadge(price);
+    void starBadgeTest(int totalPrice, String badge) {
+        String badgeName = Badge.getBadge(totalPrice);
         Assertions.assertEquals(badgeName, badge);
     }
 }
